c2d4ff50d21ab5b46040d8c5dfb668f6b2803fcc
Brandon <brandon1342568@gmail.com>
2021-09-12 18:58:05 +0000

    added antibots

---
+++ me/danny125/peroxide/modules/combat/AntiBots.java
@@ -0,0 +1,70 @@
+package me.danny125.peroxide.modules.combat;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.lwjgl.input.Keyboard;
+
+import me.danny125.peroxide.InitClient;
+import me.danny125.peroxide.Events.Event;
+import me.danny125.peroxide.Events.EventUpdate;
+import me.danny125.peroxide.Events.MotionEvent;
+import me.danny125.peroxide.modules.Module;
+import me.danny125.peroxide.utilities.IsTeammate;
+import me.danny125.peroxide.utilities.IsVillager;
+import me.danny125.peroxide.utilities.rotation.RotationUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.play.client.C03PacketPlayer;
+
+public class AntiBots extends Module {
+
+	public static ArrayList<Entity> bots = new ArrayList<Entity>();
+	
+	
+	public static List<EntityLivingBase> targets;
+	
+	public AntiBots() {
+		super("AntiBots", Keyboard.KEY_NONE, Category.COMBAT);
+	}
+	
+	public void onEvent(Event e) {
+		if(e instanceof EventUpdate) {
+			for(Object T : mc.theWorld.loadedEntityList) {
+				Entity entity = (Entity)T;
+				
+				//Invisible Check and Name Check
+				if(entity.isInvisible()) {
+					bots.add(entity);
+				}
+				
+				if(!(entity instanceof EntityMob) && !(entity instanceof EntityAnimal)) {
+					if(entity.getCustomNameTag() == "") {
+						bots.add(entity);
+					}
+				}
+				
+				//Checks for Odd ground movement, also checks if the server is faking onground, removes bots from most servers, but may trigger in a hvh
+        		if(!entity.onGround && entity.motionY == 0 || entity.isAirBorne && entity.motionY == 0) {
+        		bots.add(entity);
+        		}
+        		
+        		//Checks if entity is a villager
+        		if(IsVillager.isVillager(entity)) {
+        			bots.add(entity);
+        		}
+		
+        		//Checks if player is on the same team
+        		if(IsTeammate.isOnSameTeam(entity)) {
+        			bots.add(entity);
+        		}
+				
+			}
+	}
+	}
+}
