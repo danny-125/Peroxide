ac65c8ace74c52d0efa74c3cde9e3e8809139057
Oliver Williams <89714151+OlivesAreShit@users.noreply.github.com>
2021-09-28 22:05:52 -0500

    updated capes module class to include settings

--- me/danny125/peroxide/modules/render/Capes.java
+++ me/danny125/peroxide/modules/render/Capes.java
@@ -9,11 +9,14 @@ import me.danny125.peroxide.settings.ModeSetting;
 import net.minecraft.client.Minecraft;
 
 public class Capes extends Module {
+	
+	public static ModeSetting capeType = new ModeSetting("Cape", "Peroxide", "Peroxide", "Minecon11", "Minecon16", "Cake", "Wings");
 
 	Minecraft mc = Minecraft.getMinecraft();
 	
 	public Capes() {
 		super("Capes", Keyboard.KEY_NONE, Category.RENDER);
+		this.addSettings(capeType);
 	}
 	
 }
